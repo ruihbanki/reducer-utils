ffbf3507b34dc83e1729ed59c07997044d52347d
Rui Hiroshi Banki <ruihbanki@gmail.com>
2017-06-18 10:06:29 -0300

    Creation of the utils.js. Better readme. More tests.

--- README.md
+++ README.md
@@ -4,11 +4,29 @@ Utilities to create a new state in redux.
 
 ## Code Example
 
-...
+// state
+const state = {
+    element: {
+        {
+            type: 'DIV'
+        }
+    }
+};
+
+// create a proxy state
+const proxyState = createProxyState(state);
+const proxy = proxyState.getProxy();
+
+// change anything in proxy not change the original state
+proxy.element.type = 'SPAN';
+
+// get the new state
+const newState = proxyState.getNewState();
 
 ## Motivation
 
-...
+It is common to make some mistakes when we are creating immutable states. 
+Sometime it is necessary to write many code to create then.
 
 ## Installation
 
@@ -16,15 +34,40 @@ Utilities to create a new state in redux.
 
 ## API Reference
 
-...
+### state-proxy
+
+import createProxyState from '../src/state-proxy';
+const proxyState = createProxyState(state);
+
+// get proxy of the state
+const proxy = proxyState.proxy;                 
+
+// return the new state 
+const newState = proxyState.getNewState();      
+
+### utils
+
+findByProperty(items, propName, propValue);
+
+findById(items, id);
+
+findNestedByProperty(items, propName, propValue, propChildren = 'children');
+
+findNestedById(items, id, propChildren = 'children');
+
+deleteByProperty(items, propName, propValue);
+
+deleteById(items, id);
+
+deleteNestedByProperty(items, propName, propValue, propChildren = 'children');
+
+deleteNestedById(items, id, propChildren = 'children');
+
+logDifferences(state, newState);
 
 ## Tests
 
 `npm test`
-
-## Contributors
-
-...
 
 ## License
 
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,83 @@
+function findByProperty(items, propName, propValue) {
+    for (let item of items) {
+        if (item[propName] === propValue) {
+            return item;
+        }
+    }
+    return null;
+}
+
+function findById(items, id) {
+    return findByProperty(items, 'id', id);
+}
+
+function findNestedByProperty(items, propName, propValue, propChildren = 'children') {
+    for (let item of items) {
+        if (item[propName] === propValue) {
+            return item;
+        }
+        let children = item[propChildren];
+        if (children) {
+            let find = findNestedByProperty(children, propName, propValue, propChildren);
+            if (find) {
+                return find;
+            }
+        }
+    }
+    return null;
+}
+
+function findNestedById(items, id, propChildren = 'children') {
+    return findNestedByProperty(items, 'id', id, propChildren);
+}
+
+function deleteByProperty(items, propName, propValue) {
+    let index = -1;
+    for (let i = 0, len = items.length; i < len; i++) {
+        let item = items[i];
+        if (item[propName] === propValue) {
+            index = i;
+            break;
+        }
+    }
+    if (index > -1) {
+        items.splice(index, 1);
+        return true;
+    }
+    return false;
+}
+
+function deleteById(items, id) {
+    return deleteByProperty(items, 'id', id);
+}
+
+function deleteNestedByProperty(items, propName, propValue, propChildren = 'children') {
+    let index = -1;
+    for (let i = 0, len = items.length; i < len; i++) {
+        let item = items[i];
+        if (item[propName] === propValue) {
+            index = i;
+            break;
+        }
+        let children = item[propChildren];
+        if (children) {
+            let deleted = deleteNestedByProperty(children, propName, propValue, propChildren);
+            if (deleted > -1) {
+                return true;
+            }
+        }
+    }
+    if (index > -1) {
+        items.splice(index, 1);
+        return true;
+    }
+    return false;
+}
+
+function deleteNestedById(items, id, propChildren = 'children') {
+    return deleteNestedByProperty(items, 'id', id, propChildren);
+}
+
 function logDifferences(state, newState, level = 1) {
     if (level === 1) {
         console.log('STATE DIFFERENCE');
@@ -10,12 +90,20 @@ function logDifferences(state, newState, level = 1) {
                 console.log(Array(level + 1).join('-') + ' ' +  key);
                 if (stateValue && newStateValue) {
                     logDifferences(stateValue, newStateValue, level + 2);
-                }                
+                }          
             }
         }
     }
 }
 
 export default {
+    findByProperty,
+    findById,
+    findNestedByProperty,
+    findNestedById,
+    deleteByProperty,
+    deleteById,
+    deleteNestedByProperty,
+    deleteNestedById,
     logDifferences
 };
