009c7fa802537ffec570c59e67e8cc5946d331d6
Rui Hiroshi Banki <rui@softplan.com.br>
2018-03-22 12:56:42 -0300

    versao 0.1.3

--- .dist/src/index.js
+++ .dist/src/index.js
@@ -8,9 +8,14 @@ var _utils = require('./src/utils');
 
 var _utils2 = _interopRequireDefault(_utils);
 
+var _reducerStateProxy = require('./reducer-state-proxy');
+
+var _reducerStateProxy2 = _interopRequireDefault(_reducerStateProxy);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 exports.default = {
     createProxyState: _createProxyState2.default,
-    utils: _utils2.default
+    utils: _utils2.default,
+    reducerStateProxy: _reducerStateProxy2.default
 };
